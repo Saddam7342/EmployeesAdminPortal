131a1da644c9d9422316cb4323b296b866c16bd9
Saddam Hussain <105212372+Saddam7342@users.noreply.github.com>
2026-01-12 00:16:33 +0500

    file & folders structure

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using EmployeesAdminPortal.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeesAdminPortal.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options) 
+        //{  
+        //}
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
